3abb2de0239617a41d7b1c51c04cf0192ea24323
Arthur Philadelpho <arthur22rj@gmail.com>
2016-09-13 14:26:10 -0300

    Events Checkpoint Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var points = document.getElementsByClassName('point');
 
-var animatePoints = function() {
+var animatePoints = function(pts) {
     // These revealPoints functions give each of the elements in the points array their original position and size (since we changed the original in the landing.css styles).
     var revealPoints = function (array) {
         for(var i = 0; i < 3; i++){
@@ -13,5 +13,17 @@ var animatePoints = function() {
     revealPoints(points);
 };
 
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    var sellingPoints = document.getElementsByClassName('point')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event){
+        if (/*document.documentElement.scrollTop ||*/document.body.scrollTop >= scrollDistance) {
+             animatePoints(points);   
+         }
+    })
+}
 // animatePoints();
 // Uncomment above or call from console.
