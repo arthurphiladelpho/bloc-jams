65d71db38e7694da196e5e835e170a9af0364700
Arthur Philadelpho <arthur22rj@gmail.com>
2016-09-13 11:25:31 -0300

    Assignment Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,17 @@
- var animatePoints = function() {
-                // Create an array called points which holds all the elements that belong to the point class.
-                 var points = document.getElementsByClassName('point');
-                // These revealPoints functions give each of the elements in the points array their original position and size (since we changed the original in the landing.css styles).
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
-                 //Finally we call the functions declared above.
-                 //These will be called when the animatePoints function is called.    
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
-             };
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function() {
+    // These revealPoints functions give each of the elements in the points array their original position and size (since we changed the original in the landing.css styles).
+    var revealPoints = function (array) {
+        for(var i = 0; i < 3; i++){
+            array[i].style.opacity = 1;
+            array[i].style.transform = "scaleX(1) translateY(0)";
+            array[i].style.msTransform = "scaleX(1) translateY(0)";
+            array[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+    };
+    revealPoints(points);
+};
+
+// animatePoints();
+// Uncomment above or call from console.
