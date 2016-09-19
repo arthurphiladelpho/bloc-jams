312fc24c139d818829a775119b166cc1a1222d77
Arthur Philadelpho <arthur22rj@gmail.com>
2016-09-19 16:38:10 -0300

    DOM Play / Pause Part 2 -Complete

--- scripts/album.js
+++ scripts/album.js
@@ -46,7 +46,7 @@ var albumPicasso = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td data-song-number="'+ songNumber + '" class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -73,27 +73,112 @@ var albumPicasso = {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
 };
+
+
+
+// New code goes here :
+
+//When any of the table rows is clicked the number will become an pause icon
+    //Where is the pause icon url?
+
+// We can just give the functionality to all the trs in the 
+// document, and then check if they have the number attribute.
+
+//Loop over rows
+    //Attach a Listener to the button
+
+
+
+
+
 // Quesion: Since in line 77 we select the first element of the class, how come the code from line 84 - 86 prints out whatever table row element it hovers over?
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
+// var songNums = document.getElementsByClassName("album-song-button");
 //Button Template
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs
+ var currentlyPlayingSong = null;
+
+// findParentByClassName function
+function findParentByClassName(element, parentClass) {
+    // check if element is true
+        if(element){
+            var elementParent = element.parentElement;
+            // while element has a parent or parent doesn't belong to parentClass
+                while(elementParent.className !== parentClass && elementParent.className !== null){
+                    // Traverses up parents
+                    elementParent = elementParent.parentElement;
+                }
+                return elementParent;
+        } else {
+            alert('element doesnt exist');
+        }     
+}
+
+// Find the element that contains the class of song-item-number.
+function getSongItem(element) {
+    switch(element.className){
+        // ---- WALK THROUGH THIS CODE WITH ZACH -----
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;    
+    };    
+}
+
+ var clickHandler = function(targetElement) {
+    var songItem = getSongItem(targetElement);
+    if(currentlyPlayingSong === null){
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+ };
 
 window.onload = function() {
     setCurrentAlbum(albumKymil);
- 
     songListContainer.addEventListener('mouseover', function(event) {
         if (event.target.parentElement.className === 'album-view-song-item') {
-             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-         }
+            var songItem = getSongItem(event.target);
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
+        }
+    
     });
 
     for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
-            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-
+             var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
          });
-     }
+
+          songRows[i].addEventListener('click', function(event) {
+             clickHandler(event.target);
+         });
+    };
 };
 
 
