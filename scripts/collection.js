6244fa48a8f2aa209586d1b592437dd73307d753
Arthur Philadelpho <arthur22rj@gmail.com>
2016-09-19 22:58:31 -0300

    jQuery Collection and Album View Checkpoint Complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
- var collectionItemTemplate =
+ var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -12,15 +13,22 @@
    + '    </p>'
    + '  </div>'
    + '</div>'
-   ;
+     ;
+     return $(template);
+   };
 
-   window.onload = function() {
+    $(window).load(function() {
       // Select first element with a class of album-covers
-      var collectionContainer = document.getElementsByClassName('album-covers')[0];
+      // var collectionContainer = document.getElementsByClassName('album-covers')[0];
+      var $collectionContainer = $('.album-covers');
+      console.log($collectionContainer);
       // Ensure string is cleared
-      collectionContainer.innerHTML = '';
+      // collectionContainer.innerHTML = '';
+      $collectionContainer.empty();
       // Create 12 instances of the album cover templates
       for (var i = 0; i < 12; i++){
-         collectionContainer.innerHTML += collectionItemTemplate;
+         // collectionContainer.innerHTML += collectionItemTemplate;
+          var $newThumbnail = buildCollectionItemTemplate();
+          $collectionContainer.append($newThumbnail);         
       }
-   }
+   });  
